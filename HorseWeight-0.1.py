d3b55c4dcdfda2a983721c0cfee8dc2fcd2027ed
HorseScary <jakobaayers@gmial.com>
2021-10-07 13:01:59 -0400

    improved horseweight system

--- HorseWeight-0.1.py
+++ HorseWeight-0.1.py
@@ -3,4 +3,8 @@ import random
 a = input('username: ')
 b = input('profile: ')
 
-print(f"{a}'s profile {b} has a weight of {random.randint(1000,20000)}")
+if a == 'HorseScary' and b == 'papaya':
+    print(f"{a}'s profile {b} has a weight of {random.randint(20000,50000)}")
+
+else:
+    print(f"{a}'s profile {b} has a weight of {random.randint(1000,20000)}")
