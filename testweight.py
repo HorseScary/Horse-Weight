1146ff21b8aa4440ee037e99f537940581915290
HorseScary <jakobaayers@gmial.com>
2021-10-08 11:34:52 -0400

    added all inputs for testweight

--- testweight.py
+++ testweight.py
@@ -1,14 +1,19 @@
 from weightcalc import dungeoncalc, skillcalc, slayercalc
 
+print("Whelcome to HorseWeight!\nSince im bad at coding HorseWeight curently does not support api\nEnter your stats below:")
+
 uuid = input("UUID: ")
 f4comp = input("Floor 4 Completions: ")
 acaciacol = input("Acacia Collection: ")
 souls = input("Fairy Souls: ")
+treasure = input("Large Treasure Fished: ")
+eman = input("Tier 3 Endermen Killed: ")
+petscore = input("Pet Score: ")
+minslot = input("Minion Slots: ")
 
-
-dungeon = dungeoncalc(uuid, f4comp)
-skill = skillcalc('8805940', 221, 424)
-slayer = slayercalc(713, 224, 20)
+dungeon = dungeoncalc(str(uuid), int(f4comp))
+skill = skillcalc(str(acaciacol), int(souls), int(treasure))
+slayer = slayercalc(int(eman), int(petscore), int(minslot))
 
 print (f"Your dungeon weight is {dungeon}")
 
