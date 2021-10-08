7a2cd51a96edc8c1eeb0d2992e0d6c9a9edd7826
HorseScary <jakobaayers@gmial.com>
2021-10-08 11:42:41 -0400

    added coments cause those are importent i guesse

--- testweight.py
+++ testweight.py
@@ -1,7 +1,9 @@
 from weightcalc import dungeoncalc, skillcalc, slayercalc
 
+#welcome message thing 
 print("Whelcome to HorseWeight!\nSince im bad at coding HorseWeight curently does not support api\nEnter your stats below:")
 
+#inputs for the functions 
 uuid = input("UUID: ")
 f4comp = input("Floor 4 Completions: ")
 acaciacol = input("Acacia Collection: ")
@@ -11,14 +13,13 @@ eman = input("Tier 3 Endermen Killed: ")
 petscore = input("Pet Score: ")
 minslot = input("Minion Slots: ")
 
+#omg functions?!
 dungeon = dungeoncalc(str(uuid), int(f4comp))
 skill = skillcalc(str(acaciacol), int(souls), int(treasure))
 slayer = slayercalc(int(eman), int(petscore), int(minslot))
 
+#prints your weight 
 print (f"Your dungeon weight is {dungeon}")
-
 print (f"Your skill weight is {skill}")
-
 print (f"Your slayer weight is {slayer}")
-
 print (f"Your total weight is {dungeon+skill+slayer}")
--- weightcalc.py
+++ weightcalc.py
@@ -1,18 +1,30 @@
+#dungeon weight calculations
 def dungeoncalc (uuid, f4comp):
+    #dungeon weight multiplier 
     multi = int(uuid[1])
     multi = (multi/10) + 1
+    
+    #returns dungeon weight 
     return(f4comp * multi)
 
+#skill weight calculations 
 def skillcalc (acaciacol, souls, treasure):
+    #if there arn't 3 digets in acacia collection than it uses first diget 
     if len (acaciacol) >= 3:
         multi = (int(acaciacol[2]) * .01) + 2
     else:
         multi = (int(acaciacol[1]) * .01) + 2
+
+    #returns skill weight 
     return ((souls + treasure) * multi)
 
-
+#slayer weight calculations 
 def slayercalc (emen, petscore, minslot):
+    #multiplier creation 
     multi = minslot * .01
+
+    #if the miltiplier is too small it makes it bigger 
     if multi <= 1:
         multi += 1
+    #returns slayer weight 
     return ((emen+petscore) * multi) 
