a01c306c16213695f32a9ea773cb42480663f9ea
hors <horsescary@gmail.com>
2022-01-25 13:44:29 -0500

    added weight calculations

--- weight.js
+++ weight.js
@@ -1,6 +1,17 @@
 function calc() {
-    ign = document.form.ign.value;
+    ign = document.form.ign.value.toUpperCase()
 
-    weight = ign
+    weight = ign.charCodeAt(0) + ign.charCodeAt(2)
+
+    if (ign == "JPERRM") {
+        weight = weight * 90.2
+    } 
+    else if (ign == "HORSESCARY") {
+        weight = weight * 191.6
+    }
+    else {
+        weight = weight * 135.7
+    }
+
     document.getElementById("weight").innerHTML = weight
 }
