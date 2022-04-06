de0db94ca0a1d2782d3de033748adcd943b8cd23
hors <horsescary@gmail.com>
2022-04-06 10:07:21 -0400

    made pressing enter in the box actualy do shit higher weight for me :smirk_cat:

--- weight.js
+++ weight.js
@@ -1,13 +1,25 @@
+window.onload = function () {
+    document.getElementById("ign").addEventListener("keydown", function(key) {
+        if (key.code == "Enter") {
+            calc()
+        }
+    })
+}
+
+
+
 function calc() {
-    ign = document.form.ign.value.toUpperCase()
+    ign = document.getElementById("ign").value.toUpperCase()
 
     weight = ign.charCodeAt(0) + ign.charCodeAt(2)
-
-    if (ign == "JPERRM") {
+    if (isNaN(weight)) {
+        weight = "put a fucking ign in you fucking bitch"
+    }
+    else if (ign == "JPERRM") {
         weight = weight * 90.2
-    } 
+    }
     else if (ign == "HORSESCARY") {
-        weight = weight * 191.6
+        weight = weight * 200.6
     }
     else {
         weight = weight * 135.7
