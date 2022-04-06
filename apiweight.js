1379b3efa29f396cac86c2c34d6f719eacd07236
hors <horsescary@gmail.com>
2022-04-06 10:24:38 -0400

    added more stuff to api page

---
+++ apiweight.js
@@ -0,0 +1,12 @@
+
+window.onload = function () {
+    document.getElementById("ign").addEventListener("keydown", function(key) {
+        if (key.code == "Enter") {
+            calc()
+        }
+    })
+}
+
+function calc() {
+    
+} 
