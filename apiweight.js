41757a82535754fcabe24420fbc6019663f6fb99
horse <horsescary@gmail.com>
2022-05-24 14:49:28 -0400

    i fucking hate the fetch api

--- apiweight.js
+++ apiweight.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
     document.getElementById("ign").addEventListener("keydown", function(key) {
         if (key.code == "Enter") {
@@ -8,5 +7,14 @@ window.onload = function () {
 }
 
 function calc() {
-    
+    console.log(getProfiles("horsescary"))
 } 
+
+function getProfiles(name) {
+    fetch(`https://sky.shiiyu.moe/api/v2/profile/${name}`)
+    .then(response => response.json())
+    .then(data => profileData = (data))
+//    let profiles = await fetch (`https://sky.shiiyu.moe/api/v2/profile/${name}`)
+//    return(await profiles.text)
+    return(profileData)
+}
